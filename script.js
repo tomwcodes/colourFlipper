77fd44d36a95a508467988e309bb4d23149ce924
tomwcodes <tomwcodes@gmail.com>
2024-02-15 09:34:28 +0000

    multi digit fix

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const buttonC = document.querySelector("#buttonC");
 const result = document.querySelector("#result");
 let moveCounter = 0
 let stopInput = false
+let stopNumInput = false
 
 // Random number generator
 let randomNum = Math.floor(Math.random() * 50) + 1;
@@ -22,50 +23,58 @@ targetNumberPlace.innerText = randomNum;
 // Event Listeners
 
 button1.addEventListener("click", function(){
-    if (!stopInput){
-    result.innerText += 1;
+    if (!stopInput && !stopNumInput) {
+        result.innerText += 1;
     moveCounter += 1;
+    stopNumInput = true
     }
 });
 
 button2.addEventListener("click", function(){
-    if (!stopInput){
+    if (!stopInput && !stopNumInput){
     result.innerText += 2;
     moveCounter += 1;
+    stopNumInput = true
     }
 });
 
 button3.addEventListener("click", function(){
-    if (!stopInput){
+    if (!stopInput && !stopNumInput){
     result.innerText += 3;
     moveCounter += 1;
+    stopNumInput = true
 }});
 
 button4.addEventListener("click", function(){
-    if (!stopInput){
+    if (!stopInput && !stopNumInput){
     result.innerText += 4
     moveCounter += 1;
+    stopNumInput = true
 }});
 
 buttonPlus.addEventListener("click", function(){
     if (!stopInput){
     result.innerText += "+"
+    stopNumInput = false;
 }});
 
 buttonMultiply.addEventListener("click", function(){
     if (!stopInput){
     result.innerText += "*";
+    stopNumInput = false;
 }});
 
 buttonBack.addEventListener("click", function(){
     if (!stopInput){
     result.innerText = result.innerText.slice(0, -1);
+    stopNumInput = false;
 }});
 
 buttonC.addEventListener("click", function(){
 result.innerText = null;
 moveCounter = 0;
 stopInput = false
+stopNumInput = false;
 
 })
 
@@ -87,8 +96,3 @@ result.innerText = `You reached ${resultValue} in ${moveCounter} moves`
         result.innerText = `${resultValue} \n \n Try again`
     }
 });
-
-
-    // Log the values for debugging
-    console.log("Random Number:", randomNum);
-    console.log("Result Value:", resultValue);
